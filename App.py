6ecf50f54a8032ec916adf8e527cc9b4f3d93afe
BENSALEM Hafsa <127440075+BensalemHafsa@users.noreply.github.com>
2024-06-28 20:58:49 +0200

    Update App.py

--- App.py
+++ App.py
@@ -11,16 +11,33 @@ def predict(values):
 
 
 def main():
-    st.title("First ML Deployment")
-    st.write("Iris prediction")
-    feature1 = st.number_input("SepalLengthCm")
-    feature2 = st.number_input("SepalWidthCm")
-    feature3 = st.number_input("PetalLengthCm")
-    feature4 = st.number_input("PetalWidthCm")
-    input_data = pd.DataFrame( { "SepalLengthCm" : [feature1],
-                                 "SepalWidthCm" : [feature2],
-                                 "PetalLengthCm" : [feature3],
-                                 "PetalWidthCm" : [feature4] })
+    st.title("ML Project Deployment")
+    st.write("Bankloan Prediction")
+    feature1 = st.number_input("Age")
+    feature2 = st.number_input("Experience")
+    feature3 = st.number_input("Income")
+    feature4 = st.number_input("ZIP.Code")
+    feature5 = st.number_input("Family")
+    feature6 = st.number_input("CCAvg")
+    feature7 = st.number_input("Education")
+    feature8 = st.number_input("Mortgag")
+    feature9 = st.number_input("Securities.Account")
+    feature10 = st.number_input("CD.Account")
+    feature11 = st.number_input("Online")
+    feature12 = st.number_input("CreditCard")
+    input_data = pd.DataFrame( { "Age" : [feature1],
+                                 "Experience" : [feature2],
+                                 "Income" : [feature3],
+                                  "ZIP.Code" : [feature4],
+                                  "Family" : [feature5],
+                                  "CCAvg" : [feature6],
+                                  "Education" : [feature7],
+                                  "Mortgag" : [feature8],
+                                  "Securities.Account" : [feature9],
+                                  "CD.Account" : [feature10],
+                                  "Online" : [feature11],
+                                  "CreditCard" : [feature12],
+                                
     if st.button("Predict"):
         prediction = predict(input_data)
         st.write("Prediction : ",prediction)
